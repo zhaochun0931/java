98a4de5b706a528b1d5280ad8205f8635019df6c
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-13 14:58:43 +0800

    Update TestException.java

--- TestException.java
+++ TestException.java
@@ -1,16 +1,29 @@
 public class TestException {
     public static void main(String[] args) {
         try{
-            int divideByZero = 6 / 0;
-            System.out.println("division is " + divideByZero);
+            int a = 10;
+            int b = 0;
+            int result = a / b;
+            System.out.println("this is the try code block");
+            System.out.println("division is " + result);
             System.out.println("rest of code in try block");
+            System.out.println("");
+            System.out.println("");
 
         }
         catch (ArithmeticException e){
+            System.out.println("the is the exception code block");
             System.out.println("arithmetic exectpion=>" + e.getMessage());
+            e.printStackTrace();
+            System.out.println("");
+            System.out.println("");
+
         }
         finally {
-            System.out.println("this is the final block");
+            System.out.println("this is the final code block");
+            System.out.println("");
+            System.out.println("");
+
         }
     }
 }
