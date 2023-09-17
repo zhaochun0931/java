f5f4515cec29761a96b22db0486d57a6ead11f98
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-09-17 22:43:14 +0800

    Create DemoSerialization1.java

---
+++ java-programming/java-core-serialization/DemoSerialization1.java
@@ -0,0 +1,63 @@
+import java.io.*;
+
+class Demo implements java.io.Serializable{
+    public int a;
+    public String b;
+
+    public Demo(int a, String b){
+        this.a = a;
+        this.b = b;
+    }
+
+}
+
+class DemoSerialization1{
+    public static void main(String[] args) {
+        Demo myobj = new Demo(1,"xiaoming");
+        String myfilename = "file.txt";
+
+        try {
+            FileOutputStream fso = new FileOutputStream(myfilename);
+            ObjectOutputStream oos = new ObjectOutputStream(fso);
+            oos.writeObject(myobj);
+            oos.close();
+            fso.close();
+
+            System.out.println("Object has been serialized");
+
+        }
+        catch (IOException ex){
+            System.out.println("IOException is caught");
+        }
+
+
+        Demo object1 = null;
+        try {
+            FileInputStream fis = new FileInputStream(myfilename);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            object1 = (Demo) ois.readObject();
+            ois.close();
+            fis.close();
+
+            System.out.println("Object has been deserialized");
+            System.out.println("a = " + object1.a);
+            System.out.println("b = " + object1.b);
+
+
+
+        }
+
+        catch (IOException ex) {
+            System.out.println("IOException is caught");
+
+        }
+
+        catch (ClassNotFoundException ex){
+            System.out.println("ClassNotFoundException is caught");
+        }
+
+
+    }
+}
+
