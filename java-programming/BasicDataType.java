c133cefe4526dafe6beefd4df33076e7f38b9fda
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-09-17 13:09:41 +0800

    Create BasicDataType.java

---
+++ java-programming/BasicDataType.java
@@ -0,0 +1,54 @@
+import java.io.FileOutputStream;
+import java.sql.SQLOutput;
+
+public class BasicDataType {
+    public static void main(String[] args) {
+        System.out.println("hello basic data type");
+
+//        Byte
+        System.out.println("Byte");
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+//        Short
+        System.out.println("Short");
+        System.out.println(Short.SIZE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+//        Long
+        System.out.println("Long");
+        System.out.println(Long.SIZE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+//        Integer
+        System.out.println("Integer");
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+
+//        Float
+        System.out.println("Float");
+        System.out.println(Float.SIZE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+//        Double
+        System.out.println("Double");
+        System.out.println(Double.SIZE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+//        Character
+        System.out.println("Character");
+        System.out.println(Character.SIZE);
+        System.out.println((int)Character.MIN_VALUE);
+        System.out.println((int)Character.MAX_VALUE);
+
+
+
+
+    }
+}
