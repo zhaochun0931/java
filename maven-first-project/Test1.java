f8dccca14b7118bf1afc1a6f5ea0d0313dff79ce
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-09-27 08:19:34 +0800

    Create Test1.java

---
+++ maven-first-project/Test1.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+
+public class Test1 {
+
+    String name;
+    int age;
+
+
+
+    public void tell(){
+        System.out.println("hello test1");
+        System.out.println("the name is: " + name);
+        System.out.println("the age is: " + age + "years old");
+    }
+}
