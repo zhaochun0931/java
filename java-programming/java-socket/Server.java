1c7282ea695004aa4ae55b37d1800964430a0948
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-10-15 12:09:13 +0800

    Create Server.java

---
+++ java-programming/java-socket/Server.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+  public void run() {
+	try {
+		int serverPort = 6666;
+		ServerSocket serverSocket = new ServerSocket(serverPort);
+//		serverSocket.setSoTimeout(10000);
+		while(true) {
+			System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
+
+			Socket server = serverSocket.accept();
+			System.out.println("Just connected to " + server.getRemoteSocketAddress());
+
+			PrintWriter toClient =
+				new PrintWriter(server.getOutputStream(),true);
+			BufferedReader fromClient =
+				new BufferedReader(
+						new InputStreamReader(server.getInputStream()));
+			String line = fromClient.readLine();
+			System.out.println("Server received: " + line);
+			toClient.println("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
+		}
+	}
+	catch(UnknownHostException ex) {
+		ex.printStackTrace();
+	}
+	catch(IOException e){
+		e.printStackTrace();
+	}
+  }
+
+  public static void main(String[] args) {
+		Server srv = new Server();
+		srv.run();
+  }
+}
