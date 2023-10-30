748150c9ddf675e5afb8917b6f428c9eb1a078af
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-10-30 09:04:26 +0800

    Update App.java

--- java-programming/java-array/App.java
+++ java-programming/java-array/App.java
@@ -1,4 +1,7 @@
-public class App {
+
+import java.util.Random;
+
+public class Main {
 
     public static void main(String[] args) {
 
@@ -8,19 +11,21 @@ public class App {
         a1 = a3;
         a2 = a3;
 
+        int[] b2 = new int[5];
+        Random myrand = new Random();
 
 
-        for (int i = 0; i < a3.length; i++)
-        {
+        for (int i = 0; i < a3.length; i++) {
             System.out.println(a3[i]);
         }
 
-
         System.out.println(a1[2]);
-
         System.out.println(a2[1]);
 
-
+        for(int i = 0; i < 5; i++){
+            b2[i] = myrand.nextInt(20);
+            System.out.println(b2[i]);
+        }
 
 
     }
