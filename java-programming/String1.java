d25e8d484f405deb48014b1bc05c847883ba3922
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-10-30 08:44:12 +0800

    Create String1.java

---
+++ java-programming/String1.java
@@ -0,0 +1,20 @@
+public class String1 {
+    public static void main(String[] args) {
+
+        String a1 = new String();
+        a1 = "hello";
+
+        String a2 = new String("world");
+
+        String a3 = ("xxx");
+
+        System.out.println(a1);
+
+        System.out.println(a2);
+
+        System.out.println(a1 + " " + a2);
+
+        System.out.println(a3);
+
+    }
+}
