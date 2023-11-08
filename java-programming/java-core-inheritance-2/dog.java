2335f8ff610ed54ecd8dce4f3bda0077eaa13652
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-11-08 09:44:35 +0800

    Create dog.java

---
+++ java-programming/java-core-inheritance-2/dog.java
@@ -0,0 +1,15 @@
+package myapp;
+import myapp.animal;
+
+public class dog extends animal{
+
+
+
+
+    public static void main(String[] args) {
+
+        dog test1 = new dog();
+
+        System.out.println(test1.name);
+    }
+}
