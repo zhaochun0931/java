0d566ce0b909ddc2952803f5ce7dfafdf7c2e995
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-11-18 14:28:07 +0800

    Create Company.java

---
+++ java-programming/java-class-5/Company.java
@@ -0,0 +1,15 @@
+public class Company {
+
+    public static void main(String[] args) {
+
+        Employee m1 = new Employee("user1");
+        Employee m2 = new Employee("user2");
+        Employee m3 = new Employee("user3");
+
+        m1.start();
+        m2.start();
+        m3.start();
+
+//        System.out.println();
+    }
+}
