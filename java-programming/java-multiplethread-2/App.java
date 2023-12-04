92221f7f1a985cf20df2bd14b63c2ea5c9e50f40
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2023-12-04 20:40:57 +0800

    Update App.java

--- java-programming/java-multiplethread-2/App.java
+++ java-programming/java-multiplethread-2/App.java
@@ -1,1 +1,30 @@
+class MultiThreadDemo implements Runnable{
+    public void run(){
+        try{
+            System.out.println("Thread " + Thread.currentThread().getId() + " is running");
 
+        }
+        catch (Exception e){
+            System.out.println("exception is caught");
+
+        }
+    }
+}
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int n = 10;
+        for(int i = 0; i < n; i++){
+
+            Thread xx  = new Thread(new MultiThreadDemo());
+            xx.start();
+
+        }
+    }
+}
