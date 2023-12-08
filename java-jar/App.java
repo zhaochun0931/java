036caf0e64b4b5a21d843e5b65177c7aafdf9f81
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2023-12-08 11:38:39 +0800

    Update App.java

--- java-jar/App.java
+++ java-jar/App.java
@@ -6,8 +6,17 @@
  */
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
+        
+        while (true){
+            
+            Date current_now = new Date();
+            System.out.println("The current time is: " + current_now);
+            Thread.sleep(1000);
+
+
+        }
     }
 }
