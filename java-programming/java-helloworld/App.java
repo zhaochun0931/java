b16073666206245a9c59ed1fee8acfb79b0f1070
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-04-03 14:06:33 +0800

    Create App.java

---
+++ java-programming/java-helloworld/App.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.lang.Thread;
+import java.time.*;
+
+
+public class App {
+    public static void main(String[] args) {
+
+        try{
+            LocalTime localtime = LocalTime.now();
+            System.out.println(localtime);
+            System.out.println("This java program will pause for 10 seconds");
+
+
+            Thread.sleep(10000);
+            LocalTime localtime2 = LocalTime.now();
+            System.out.println(localtime2);
+            System.out.println("hello world");
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
