d250d0b80bf111de3103e276d345c8e90f834497
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-04-04 11:55:34 +0800

    Update App.java

--- java-jar/App.java
+++ java-jar/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "It will print the current time every sec." );
         
         while (true){
             
