97f261b8d0b132ab69d5d980cc4cde91d4b28b4c
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-05-29 19:08:40 +0800

    Create app.java

---
+++ java-programming/java-ArrayList/app.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+ArrayList<String> cars = new ArrayList<String>(); 
+
+
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
