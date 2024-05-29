f56d4a8c7c2529f47b9da43942591cdb6f57859b
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-05-29 19:22:54 +0800

    Update App2.java

--- java-programming/java-thread/App2.java
+++ java-programming/java-thread/App2.java
@@ -1,14 +1,14 @@
 public class App2 implements Runnable {
-  
+
   public static void main(String[] args) {
-    App2 obj = new Main();
+    App2 obj = new App2();
     Thread thread = new Thread(obj);
     thread.start();
     System.out.println("This code is outside of the thread");
   }
-  
+
   public void run() {
     System.out.println("This code is running in a thread");
   }
-  
+
 }
