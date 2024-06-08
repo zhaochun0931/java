98e66400877b9b07b430f876975d6a68107d1e8e
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-06-08 19:27:13 +0800

    Update HelloWorldApplication.java

--- springboot-quick-start/HelloWorldApplication.java
+++ springboot-quick-start/HelloWorldApplication.java
@@ -1,11 +1,32 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
 @SpringBootApplication
+@RestController
+
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
+
+
+    @RequestMapping("/")
+    public String helloworld(){
+        return "hello world from Spring Boot";
+
+    }
+
+    @RequestMapping("/goodbye")
+    public String helloWorld(){
+        return "Goodbye from Spring Boot";
+    }
+
 
 }
