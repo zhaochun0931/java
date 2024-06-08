7d6bcd25f2381467531eb705c84909061873df09
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-06-08 19:38:14 +0800

    Update HelloworldApplication.java

--- spring/HelloworldApplication.java
+++ spring/HelloworldApplication.java
@@ -2,15 +2,13 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.time.LocalTime;
 import java.time.LocalDateTime;
-
-
 
 
 @SpringBootApplication
@@ -18,9 +16,9 @@ import java.time.LocalDateTime;
 
 public class HelloworldApplication {
 
-        public static void main(String[] args) {
-                SpringApplication.run(HelloworldApplication.class, args);
-        }
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
 
             @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
@@ -29,6 +27,18 @@ public class HelloworldApplication {
 
       return String.format("Hello  %s! " + currentTime, name);
     }
+
+    @RequestMapping("/")
+    public String helloworld(){
+        return "hello world from Spring Boot";
+
+    }
+
+    @RequestMapping("/goodbye")
+    public String helloWorld(){
+        return "Goodbye from Spring Boot";
+    }
+
 
 
 }
