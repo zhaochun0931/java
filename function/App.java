d778429d999195979c5019d5c7c1a6cafafd055a
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-11-13 23:11:03 +0800

    Create App.java

---
+++ function/App.java
@@ -0,0 +1,11 @@
+public class Example {
+
+    // This is a method (a "function" in other languages)
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        greet("Alice");
+    }
+}
