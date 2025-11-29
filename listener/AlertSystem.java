4cff6fe10e0591cacb0bd4a845b310053af895ac
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-11-29 18:54:57 +0800

    Delete listener/AlertSystem.java

--- listener/AlertSystem.java
+++
@@ -1,6 +0,0 @@
-public class AlertSystem implements TemperatureChangeListener {
-    @Override
-    public void onTemperatureChange(int newTemperature) {
-        System.out.println("ALERT! Temperature changed to: " + newTemperature + "°C");
-    }
-}
